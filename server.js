2f16c3bfc93e242bdaa9634c978d4cf29cb4207d
patrickmilnes <pjamilnes@gmail.com>
2020-03-10 13:39:16 +0000

    Added public folder with index.html and set it in the server code

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static("public"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port);
+console.log("localhost:8080");
