b63433c5a7056b2c40a5ddf42ddb0f8c4d2dc844
patrickmilnes <pjamilnes@gmail.com>
2020-04-27 18:15:14 +0100

    Changed password

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(express.static("public"));
 app.listen(port);
 console.log("http://localhost:8080");
 
-const { Client } = require('pg')
+const { Client } = require('pg');
 const client = new Client({
     user: "up899929",
-    password: "postgres",
+    password: "root",
     host: "localhost",
     port: 5432,
     database: "test"
