dafcd21306b4d693fa4d89092a8e6358a327a628
patrickmilnes <pjamilnes@gmail.com>
2020-04-27 17:50:31 +0100

    Added pg connection

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const pouchDB = require('pouchdb');
 const app = express();
 const port = 8080;
 
@@ -7,3 +6,17 @@ app.use(express.static("public"));
 
 app.listen(port);
 console.log("http://localhost:8080");
+
+const { Client } = require('pg')
+const client = new Client({
+    user: "up899929",
+    password: "raspberry",
+    host: "localhost",
+    port: 5432,
+    database: "test"
+});
+
+client.connect()
+.then(() => console.log("Connection Successful"))
+.catch(e => console.log("ERROR"))
+.finally(() => client.end());
